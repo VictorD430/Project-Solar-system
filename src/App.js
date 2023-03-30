f0ff9513442bed6ae8d459a373f6b19db796cf07
Victor Duque <victor.duque11@hotmail.com>
2023-03-30 15:31:20 -0300

    componente Header criado e imports feitos no App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './App.css';
+import Header from './components/Header';
 // iniciando projeto
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+        <Header />
+    );
   }
 }
 
