3a858e822a194456c0b5d438828f45263888b0ff
Victor Duque <victor.duque11@hotmail.com>
2023-03-30 16:01:14 -0300

    criado o componente Title e importado title no App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
+import Title from './components/Title';
 // iniciando projeto
 
 class App extends React.Component {
