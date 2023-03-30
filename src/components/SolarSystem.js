70e55811a1dc07c3d626c39876a051acbae0fc81
Victor Duque <victor.duque11@hotmail.com>
2023-03-30 17:10:47 -0300

    criada a listagem de planetas no componente SolarSystem

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <div className="planets">
+          {planets.map((planeta) => {
+            const { name, image } = planeta;
+            return (
+              <PlanetCard
+                key={ name }
+                planetName={ name }
+                planetImage={ image }
+              />
+            );
+          })}
+
+        </div>
       </div>
     );
   }
