49b8a2dc4c228e7f109ecf5e83e809d75f59a18c
Victor Duque <victor.duque11@hotmail.com>
2023-03-30 16:59:26 -0300

    adicionada a tag p do componente planetCard

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -6,7 +6,8 @@ class PlanetCard extends Component {
     const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <img src={ planetImage } alt={ `Planet ${planetName}` } />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{planetName}</p>
       </div>
     );
   }
